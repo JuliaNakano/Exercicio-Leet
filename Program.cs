36bac2ac71d1920e1a8a1fe719463d91b5e395f8
JuliaNakano <jumartinsnakano@gmail.com>
2021-09-21 23:28:09 -0300

    Solução do exercício LEET

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nDigite o texto que será traduzido...:\n");
+            Console.WriteLine("");
+            Console.WriteLine(
+                Console.ReadLine()
+                .Replace("A", "4")
+                .Replace("a", "4")
+                .Replace("E", "3")
+                .Replace("e", "3")
+                .Replace("I", "1")
+                .Replace("i", "1")
+                .Replace("L", "1")
+                .Replace("O", "0")
+                .Replace("o", "0")
+                .Replace("T", "7")
+                .Replace("t", "7")
+                .Replace("S", "5")
+                .Replace("s", "5")
+            );
+            Console.WriteLine("");
+        }
+    }
+}
